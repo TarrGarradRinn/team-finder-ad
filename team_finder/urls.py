926c2b28ebf28c375376d7a3ba670282bcc1b9f4
TarrGarradRinn <shevchenko.nikita.06@inbox.ru>
2026-06-21 16:59:16 +0400

    Made base urls

--- team_finder/urls.py
+++ team_finder/urls.py
@@ -5,5 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-
+    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('projects:project_list')),
+    path('projects/', include('projects.urls')),
+    path('users/', include('user.urls')),
 ]
